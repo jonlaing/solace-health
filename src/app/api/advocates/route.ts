2756715ab729da71ee6454d698c0b80d4fc29f82
Jon Laing <jon@jonlaing.io>
2025-07-15 21:33:05 -0400

    SLC-5: add query params to advocates endpoint

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,12 +1,36 @@
-import db from "../../../db";
-import { advocates } from "../../../db/schema";
 import { advocateData } from "../../../db/seed/advocates";
 
-export async function GET() {
+export async function GET(request: Request) {
   // Uncomment this line to use a database
   // const data = await db.select().from(advocates);
 
-  const data = advocateData;
+  const { searchParams } = new URL(request.url);
+  const query = searchParams.get("query");
+  const page = searchParams.get("page");
 
-  return Response.json({ data });
+  // filter advocates
+  const filteredAdvocates = advocateData.filter((advocate) => {
+    if (!query) return true;
+
+    const hasMatchingSpecialties = advocate.specialties.some((specialty) =>
+      specialty.includes(query)
+    );
+
+    return (
+      advocate.firstName.includes(query) ||
+      advocate.lastName.includes(query) ||
+      advocate.city.includes(query) ||
+      advocate.degree.includes(query) ||
+      hasMatchingSpecialties ||
+      advocate.yearsOfExperience === parseInt(query, 10)
+    );
+  });
+
+  // Implement pagination
+  const pageNumber = page ? parseInt(page, 10) : 0;
+  const pageSize = 10;
+  const startIndex = pageNumber * pageSize;
+  const endIndex = startIndex + pageSize;
+
+  return Response.json(filteredAdvocates.slice(startIndex, endIndex));
 }
